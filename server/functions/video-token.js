767f9b8c8e589fea04513bb43b3fab54407eee34
Christopher Najafi <chris.najafi@gmail.com>
2021-03-20 16:59:53 -0400

    Added server functions.

---
+++ server/functions/video-token.js
@@ -0,0 +1,24 @@
+exports.handler = function (context, event, callback) {
+  const TWILIO_ACCOUNT_SID = context.ACCOUNT_SID;
+  const TWILIO_API_KEY = context.API_KEY;
+  const TWILIO_API_SECRET = context.API_SECRET;
+  const AccessToken = Twilio.jwt.AccessToken;
+
+  const accessToken = new AccessToken(
+    TWILIO_ACCOUNT_SID,
+    TWILIO_API_KEY,
+    TWILIO_API_SECRET
+  );
+
+  accessToken.identity = event.identity;
+
+  const VideoGrant = AccessToken.VideoGrant;
+
+  const videoGrant = new VideoGrant({
+    room: event.room,
+  });
+
+  accessToken.addGrant(videoGrant);
+
+  callback(null, { token: accessToken.toJwt() });
+};
